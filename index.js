1a0544a0e1cd1cef1e2314247c65e14080a288c3
Main <main@Karens-MacBook-Air.local>
2023-03-29 11:55:58 +1100

    Added a submit request and chenge request, moved the request to a separate function

--- index.js
+++ index.js
@@ -3,22 +3,14 @@ var WEATHER_API_KEY = "0cf69d7b51047b5bd691266f2e26e463";
 var form = $("#form");
 var city = $("#cities").val();
 
-
-
-$(function(){
-    form.on("submit", function(event){
-        event.preventDefault();
-
-       
-    })
-})
-$.ajax({
+function request() {
+  $.ajax({
     type: "get",
     url: `https://api.openweathermap.org/data/2.5/weather?q=${$(
       "#cities"
     ).val()}&units=metric&appid=${WEATHER_API_KEY}`,
     success: function (data) {
-      console.log(data);
+      console.log("change", data);
 
       $("#temp").empty().append(data.main.temp);
       $("#feels").empty().append(data.main.feels_like);
@@ -34,3 +26,22 @@ $.ajax({
         );
     },
   });
+}
+
+request();
+
+$(function () {
+  form.on("change", function (event) {
+    event.preventDefault();
+
+    request();
+  });
+});
+
+$(function () {
+  form.on("submit", function (event) {
+    event.preventDefault();
+
+    request();
+  });
+});
